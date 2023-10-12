f8d1465a2a455ae02a686cba363dbdc212c81a22
Jefersonnnn <jeferson_m94@hotmail.com>
2023-10-11 21:44:23 -0300

    initial commit

---
+++ src/test/java/com/jm/jamesapp/JamesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.jamesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
